864d5250171b0da12c1944000ab9218843282520
Ayhan Dániel <ayhand@regens.hu>
2017-09-19 19:16:45 +0200

    moved classes to separate files

---
+++ ConsoleApplication1/ConsoleApplication1/DailyWork.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+	public class DailyWork
+	{
+		public TimeSpan Balance = TimeSpan.Zero;
+		public WorkEvent[] Events = new WorkEvent[0];
+		public static DailyWork FromWorkEvents(IEnumerable<WorkEvent> events)
+		{
+			var work = new DailyWork();
+			TimeSpan balance = TimeSpan.FromHours(-8);
+			List<WorkEvent> filteredEvents = new List<WorkEvent>();
+
+			DateTime? lastSignin = null;
+			foreach( var e in events )
+			{
+				if( lastSignin == null && e.Type == EventType.Arrival )
+				{
+					lastSignin = e.Time;
+					filteredEvents.Add(e);
+				}
+				else if( e.Type == EventType.Departure )
+				{
+					balance += (e.Time - lastSignin.Value);
+					lastSignin = null;
+					filteredEvents.Add(e);
+				}
+			}
+			if( lastSignin != null )
+			{
+				filteredEvents.Remove(filteredEvents.Last());
+			}
+
+			return new DailyWork()
+			{
+				Balance = balance,
+				Events = filteredEvents.ToArray()
+			};
+		}
+	}
+}
--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -1,11 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Threading;
-using System.IO;
 using System.Diagnostics;
+using System.Linq;
 
 namespace ConsoleApp1
 {
@@ -13,12 +9,16 @@ namespace ConsoleApp1
 	{
 		static void Main(string[] args)
 		{
-			Tests.RunTests();
+			//Tests.RunTests();
 
 			EventLog securityLog = GetSecurityLog();
 			List<EventLogEntry> eventList = GetEvents(securityLog);
 			WriteToLog(eventList);
 			eventList.ForEach((e) => Console.WriteLine($"category: {e.Category}, type: {e.EntryType}, id: {e.InstanceId}, time1: {e.TimeGenerated}, type: {GetEventType(e.InstanceId)}"));
+			Console.WriteLine();
+			var wt = new WorkTimes();
+			wt.AddWorks(eventList.Select(e => WorkEvent.FromLogEntry(e)).ToList());
+
 			Console.ReadLine();
 		}
 
@@ -34,7 +34,7 @@ namespace ConsoleApp1
 		{
 			var logs = EventLog.GetEventLogs();
 			var securityLog = logs.Where(l => l.Log == "Security").FirstOrDefault();
-			if (securityLog == null)
+			if( securityLog == null )
 				throw new Exception("securitylog null");
 			return securityLog;
 		}
@@ -56,105 +56,11 @@ namespace ConsoleApp1
 		{
 			long[] loginIds = new[] { 4624L, 4648L, 4801L, };
 			long[] logoutIds = new[] { 4634L, 4800L, 4647L, };
-			if (loginIds.Contains(id))
+			if( loginIds.Contains(id) )
 				return EventType.Arrival;
-			if (logoutIds.Contains(id))
+			if( logoutIds.Contains(id) )
 				return EventType.Departure;
 			return EventType.Unknown;
 		}
-	}
-
-	public class WorkTimes
-	{
-		public TimeSpan Balance { get; set; } = TimeSpan.Zero;
-		public DailyWork[] DailyWorks { get; set; } = new DailyWork[0];
-		public void AddWorks(IEnumerable<WorkEvent> events)
-		{
-			var lastWorkTime = DailyWorks.LastOrDefault()?.Events?.LastOrDefault()?.Time ?? DateTime.MinValue;
-			var filteredEvents = FilterSort(events, lastWorkTime, GetLastMidnight()).ToList();
-			var newDailyWorks = new List<DailyWork>();
-			newDailyWorks.AddRange(DailyWorks);
-
-			while (filteredEvents.Count != 0)
-			{
-				var firstEventTime = filteredEvents[0].Time;
-				var currentDayEvents = filteredEvents.Where(e => (e.Time.DayOfYear == firstEventTime.DayOfYear)).ToList();
-				newDailyWorks.Add(DailyWork.FromWorkEvents(currentDayEvents));
-				foreach (var e in currentDayEvents)
-				{
-					filteredEvents.Remove(e);
-				}
-			}
-		}
-		
-		public static DateTime GetLastMidnight()
-		{
-			DateTime result = DateTime.Now;
-			result -= result.TimeOfDay;
-			return result;
-		}
-
-		public static IEnumerable<WorkEvent> FilterSort(IEnumerable<WorkEvent> events, DateTime from, DateTime to)
-		{
-			return events.Where(e => from < e.Time && e.Time < to).OrderBy(e => e.Time);
-		}
-	}
-
-	public class DailyWork
-	{
-		public TimeSpan Balance = TimeSpan.Zero;
-		public WorkEvent[] Events = new WorkEvent[0];
-		public static DailyWork FromWorkEvents(IEnumerable<WorkEvent> events)
-		{
-			var work = new DailyWork();
-			TimeSpan balance = TimeSpan.FromHours(-8);
-			List<WorkEvent> filteredEvents = new List<WorkEvent>();
-
-			DateTime? lastSignin = null;
-			foreach (var e in events)
-			{
-				if (lastSignin == null && e.Type == EventType.Arrival)
-				{
-					lastSignin = e.Time;
-					filteredEvents.Add(e);
-				}
-				else if (e.Type == EventType.Departure)
-				{
-					balance += (e.Time - lastSignin.Value);
-					lastSignin = null;
-					filteredEvents.Add(e);
-				}
-			}
-			if (lastSignin != null)
-			{
-				filteredEvents.Remove(filteredEvents.Last());
-			}
-
-			return new DailyWork()
-			{
-				Balance = balance,
-				Events = filteredEvents.ToArray()
-			};
-		}
-	}
-
-	public class WorkEvent
-	{
-		public EventType Type = EventType.Unknown;
-		public DateTime Time = DateTime.MinValue;
-		public static WorkEvent FromLogEntry(EventLogEntry entry)
-		{
-			WorkEvent workEvent = new WorkEvent();
-			workEvent.Time = entry.TimeGenerated;
-			workEvent.Type = Program.GetEventType(entry.InstanceId);
-			return workEvent;
-		}
-	}
-
-	public enum EventType
-	{
-		Arrival,
-		Departure,
-		Unknown
 	}
 }
